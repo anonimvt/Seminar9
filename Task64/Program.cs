865e800f7284e8910a6814e0e0d4503733113f38
anonimvt <rinat.madyarov.08@gmail.com>
2023-03-06 11:31:12 +0400

    Seminar9

---
+++ Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int n = InputInt("Введите положительное число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Вы ввели не положительное число.");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int m = InputInt("Введите число M: ");
+int n = InputInt("Введите число N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+
+
+int InputInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
